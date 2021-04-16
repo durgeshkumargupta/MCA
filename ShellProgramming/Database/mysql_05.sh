0d87dbb3cdecf7245fc229d4177b91b3278bccad
Durgesh <47785309+durgeshkumargupta@users.noreply.github.com>
2021-04-16 19:36:57 +0530

    Update mysql_05.sh

--- ShellProgramming/Database/mysql_05.sh
+++ ShellProgramming/Database/mysql_05.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-mysql -u RVCE -proot <<MY_QUERY
+mysql -u root -proot <<MY_QUERY
 use RVCE;
 
-insert into Authors(name) values('william shakespeare')
+insert into Authors(name) values('william shakespeare');
 insert into Authors(name) values('Charles Dickens');
 insert into Authors(name) values('Jane Austen');
-insert into Authors values('George Orwell');
-insert into Authors values('Ossecan Wilde');
+insert into Authors(name) values('George Orwell');
+insert into Authors(name) values('Ossecan Wilde');
 
 MY_QUERY
