425592072ceca2b9a52bc687a7b35deb192a5468
Durgesh <47785309+durgeshkumargupta@users.noreply.github.com>
2021-05-25 17:04:50 +0530

    Add files via upload

---
+++ java/Online_Class/multiple_person_details/PersonTest.java
@@ -0,0 +1,28 @@
+package multiple_person_details;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+
+public class PersonTest
+{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        List<Person> personList=new ArrayList<>();
+
+        for (int i=0;i<2;i++)
+        {
+            Person person=new Person();
+            System.out.println("Enter Person name:");
+            person.setName(sc.nextLine());
+            System.out.println("Enter age:");
+            person.setAge(sc.nextInt());
+            System.out.println("Enter Address:");
+            sc.nextLine();
+            person.setAddress(sc.nextLine());
+            personList.add(person);
+        }
+        System.out.println(personList);
+
+    }
+}
