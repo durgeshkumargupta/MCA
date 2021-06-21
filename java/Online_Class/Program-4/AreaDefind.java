eb4ddb1bac5bc06c705a244c7943e1ab9116cedf
Durgesh <47785309+durgeshkumargupta@users.noreply.github.com>
2021-06-21 16:44:31 +0530

    Add files via upload

---
+++ java/Online_Class/Program-4/AreaDefind.java
@@ -0,0 +1,18 @@
+package geometry;
+
+public class AreaDefind
+{
+    public Double square(Double a)
+    {
+        return a*a;
+    }
+    public Double triangle(Double base,Double height)
+    {
+        return (base*height)/2;
+    }
+    public Double circle(Double radius)
+    {
+        return 2*3.14*radius;
+    }
+}
+
---
+++ java/Online_Class/Program-4/TestArea.java
@@ -0,0 +1,54 @@
+package shap;
+import geometry.AreaDefind;
+
+import java.util.Scanner;
+
+public class TestArea
+{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        AreaDefind obj=new AreaDefind();
+
+        while(true)
+        {
+            System.out.println("1.Square of Area");
+            System.out.println("2.Triangle of Area");
+            System.out.println("3.Circle of Area");
+            System.out.println("4.Exit");
+
+            System.out.println("Enter Your choice=");
+            int ch=sc.nextInt();
+            switch (ch)
+            {
+                case 1:
+                    System.out.println("Enter sides of square=");
+                    Double a=sc.nextDouble();
+                    System.out.println("Square of Area="+obj.square(a));
+                    break;
+                case 2:
+                    System.out.println("Enter Base=");
+                    Double base=sc.nextDouble();
+                    System.out.println("Enter Height=");
+                    Double height=sc.nextDouble();
+                   System.out.println("Triangle of Area="+obj.triangle(base,height));
+                    break;
+                case 3:
+                    System.out.println("Enter Radius=");
+                    Double radius=sc.nextDouble();
+                    System.out.println("Circle of Area="+obj.circle(radius));
+                    break;
+                case 4:
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Invalid input please try again !!");
+            }
+        }
+    }
+}
+/*
+compile:javac -d . AreaDefind.java
+compile:javac -d . TestArea.java
+Run:java shap.TestArea
+*/
